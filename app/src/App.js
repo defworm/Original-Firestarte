07cb90ea7883dc8811d04f1ed86f28b3fb2d63bb
Cody Cork <codycork.sfg@outlook.com>
2022-12-10 11:15:50 -0600

    merge

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="navbar">
+          <NavBar />
+        </div>
+
+        <div>
+          <h1 id="title">The 1 Firestarters</h1>
+        </div>
+        <div id="welcome">
+          <h2>Welcome to the #1 firestarter you need to use!</h2>
+        </div>
       </header>
     </div>
   );
